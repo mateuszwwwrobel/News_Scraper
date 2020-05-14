cb322a9b2a25beac102b10a6db007dbd633d32de
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-05-14 14:02:49 +0100

    finish wykop page

--- core/views.py
+++ core/views.py
@@ -56,47 +56,31 @@ class WykopView(View):
             wykop_data = wykop_response.text
             wykop_soup = BeautifulSoup(wykop_data, 'html.parser')
             
-            #First part of the SOUP with image and title:
+            #SOUP with url and title:
             first_part_of_soup = wykop_soup.find_all("a", {"rel": 'noopener'})
             
-            #<a> tag with <img> tag which include image link and article title.
-            shorter_first_part_of_soup = list(itertools.islice(first_part_of_soup,0,None,3))
+            #<a> tag with href and title.
+            shorter_first_part_of_soup = list(itertools.islice(first_part_of_soup,1,None,3))
             ready_first_part_of_soup = list(shorter_first_part_of_soup[3:])
             
             wykop_final_elements = []
 
             for items in ready_first_part_of_soup:
-                wykop_title = items.find("img").get('alt')
-                wykop_image = items.find('img').get('data-original')
-                
-                wykop_final_elements.append((wykop_title, wykop_image,))
+                wykop_link = items['href']
+                wykop_title = items['title']
 
-
-            #Second part of the SOUP with image and title:
-            second_part_of_the_soup = wykop_soup.find_all('div', {'class': 'media-content'})
-
-            full_wykop_final_elements2 = []
-
-            for items in second_part_of_the_soup:
-                wykop_url = items.find('a').get('href')
-                
-
-                full_wykop_final_elements2.append(wykop_url,)
-
-                wykop_final_elements2 = list(full_wykop_final_elements2[4:])
+                wykop_final_elements.append((wykop_link, wykop_title,))
             
-            
-            del full_wykop_final_elements2[10]
-            
-            
+
             context = {
                 'wykop_final_elements': wykop_final_elements,
-                'wykop_final_elements2': wykop_final_elements2,
             }
-
+            
             
             return render(self.request, 'wykop.html', context,)
 
         except ObjectDoesNotExist:
             messages.warning(self.request, "You do not have an active order.")
             return redirect("/")
+
+
