28c95c3e24494ca531ce6841dd726fa6af021984
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-27 08:56:19 +0000

    add classmethods

--- core/models.py
+++ core/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 portals = (
     ('Benchmark.pl', 'Benchmark.pl'),
-    ('Wykop.pl', 'Wykop.pl'),
+    ('boardgamesgeek.com', 'boardgamesgeek.com'),
     ('Tojuzbylo.pl', 'Tojuzbylo.pl'),
     ('Zwiadowca Historii', 'Zwiadowca Historii'),
     ('Computer World', 'Computer World'),
+    ('InfoWorldPython.com', 'InfoWorldPython.com'),
+    ('RealPython.com', 'RealPython.com'),
+
 )
 
 
@@ -18,3 +21,27 @@ class Article(models.Model):
 
     def __str__(self):
         return f'{self.portal} - {self.title}'
+
+    @classmethod
+    def check_if_article_already_exist(cls, posts_list, portal) -> None:
+        all_articles = Article.objects.filter(portal=portal)
+        article_list = []
+        [article_list.append(article.url) for article in all_articles]
+
+        for post in posts_list:
+            if post[0] in article_list:
+                continue
+            else:
+                cls.save_article(post[1], post[0], portal)
+
+    @classmethod
+    def save_article(cls, title, url, portal) -> None:
+        if title == '' or url == '':
+            pass
+        else:
+            article = Article(
+                portal=portal,
+                title=title,
+                url=url,
+            )
+            article.save()
